074be9f9c615ed8e8fb0eaeb598a013f4a2274d3
Jose <al_jllaveroi@iessierradelasvillas.org>
2022-10-17 10:29:55 +0200

    Ejercicio 9

--- Ejercicio 9.py
+++ Ejercicio 9.py
@@ -1,36 +1,14 @@
 #Ejercicio 9. Algoritmo que pida tres números y los muestre ordenados (de mayor a menor);
-num1= 0
-num2= 0
-num3= 0
+
+vNum= []
 
 num1= (int) (input("Dime un número: \n"))
 num2= (int) (input("Dime otro número: \n"))
 num3= (int) (input("Dime otro número: \n"))
 
-if (num1>num2 and num1>num3):
-    mayor = num1
+vNum.append(num1)
+vNum.append(num2)
+vNum.append(num3)
+vNum.sort(reverse=True)
 
-    if (num2>num3):
-     mediano = num2
-     menor = num3
-    else:
-     mediano = num3
-     menor = num2
-elif (num2>num1 and num2>num3):
-    mayor = num2
-    if (num1>num3):
-     mediano = num1
-     menor = num3
-    else:
-     mediano = num3
-     menor = num1
-else:
-    mayor = num3
-if (num2>num1):
-    mediano = num2
-    menor = num1
-else:
-     mediano = num1
-     menor = num2
-
-print(f"{mayor} > {mediano} > {menor}")
+print(vNum)
